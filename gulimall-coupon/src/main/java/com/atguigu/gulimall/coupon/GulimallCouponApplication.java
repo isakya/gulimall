f1ef419bb1cf3fcdceffce3a2a3ddc62e86717c3
isakya <1016361268@qq.com>
2023-05-17 03:46:49 +0800

    引入SpringCloud-Alibaba和Nacos注册中心

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -3,7 +3,9 @@ package com.atguigu.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient // 开启服务注册发现的客户端
 @SpringBootApplication
 @MapperScan("com.atguigu.gulimall.coupon.dao")
 public class GulimallCouponApplication {
