228516c0c766eb09f4b233def8acdce4e9cfdb4f
isakya <1016361268@qq.com>
2023-05-17 12:44:32 +0800

    引入Feign并测试远程调用

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.member.dao")
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign") // 开启远程调用接口
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
