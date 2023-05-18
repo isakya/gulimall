85bf1885d1ef4f7c4bf39c34b910d2a6b20b5d43
isakya <1016361268@qq.com>
2023-05-19 07:02:21 +0800

    商品服务-API-三级分类-删除-逻辑删除

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -50,6 +50,15 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return entities;
     }
 
+    // 删除方法
+    @Override
+    public void removeMenuByIds(List<Long> asList) {
+        //TODO 1. 检查当前删除的菜单是否被别的地方引用
+
+        // 逻辑删除
+        baseMapper.deleteBatchIds(asList);
+    }
+
     /**
      * 递归查找所有菜单的子菜单
      */
