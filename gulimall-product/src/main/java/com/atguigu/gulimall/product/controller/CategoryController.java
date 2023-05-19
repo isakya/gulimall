250a5cb92acfbbc34791268a271f909d973b12dd
isakya <1016361268@qq.com>
2023-05-19 23:20:51 +0800

    更新菜单排序接口

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -50,7 +50,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -61,6 +61,16 @@ public class CategoryController {
     public R save(@RequestBody CategoryEntity category){
 		categoryService.save(category);
 
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update/sort")
+    //@RequiresPermissions("product:category:update")
+    public R updateSort(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category));
         return R.ok();
     }
 
